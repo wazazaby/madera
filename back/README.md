058f54b1890cc5638ea2f3e61878e48e78a7d70b
Teddy <megareda1998@gmail.com>
2021-02-21 23:01:11 +0100

    Ajout TODO + schemas

--- back/README.md
+++ back/README.md
@@ -9,3 +9,9 @@ Madera API
 
 ## Generate Prisma client
 `npm run pgen`
+
+## TODO
+* Créer les fichiers qui stockeront les handlers
+* Créer le handler du `onRequest` pour la validation du JWT
+* Gérer l'auth de l'admin / commercial (voir pour gérer sous forme de middleware)
+* Passer la lib sous cette forme -> /back/lib/core, /back/lib/entities
--- back/lib/administrator/schemas.js
+++ back/lib/administrator/schemas.js
@@ -1,4 +1,25 @@
 'use strict';
+export const create = {
+    schema: {
+        body: {
+            type: 'object',
+            required: [
+                'firstName',
+                'lastName',
+                'email',
+                'password'
+            ],
+            properties: {
+                firstName: { type: 'string' },
+                lastName: { type: 'string' },
+                email: { type: 'string' },
+                password: { type: 'string' }
+            },
+            required: ['firstName', 'lastName', 'email', 'password']
+        }
+    }
+};
+
 export const login = {
     body: {
         type: 'object',
